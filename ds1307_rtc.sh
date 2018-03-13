9535ba784b873d28a1fa36b8e875b878b5594c13
Marc Oggier <marc.oggier@megavolts.ch>
2018-03-13 09:20:04 -0800

    Update ds1307_rtc.sh

--- ds1307_rtc.sh
+++ ds1307_rtc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo -e ".. installing I2C RTC for Arch Linux ARM"
+echo -e ". Installing I2C RTC for Arch Linux ARM"
 echo -e ""
 
 pacman -S --needed --noconfirm i2c-tools
 echo -e ".. activating I2C devicetree in /boot/config.txt"
-sed --in-place 's/^#device_tree_param=i2c_arm=on/device_tree_param=i2c_arm=on/' "/boot/config.txt"
+echo 'device_tree_param=i2c_arm=on' >> /etc/config.txt
 
 #-----------------------------------------------------------
 echo -e ""
 echo -e ".. adding i2c and rtc modules to the conf"
 echo "rtc-ds1307" >> /etc/modules-load.d/rtc.conf
 echo "i2c-dev"    >> /etc/modules-load.d/rtc.conf
-sort /etc/modules-load.d/rtc.conf | uniq > /etc/modules-load.d/rtc.conf
+sort -u /etc/modules-load.d/rtc.conf -o /etc/modules-load.d/rtc.conf 
 
 #-----------------------------------------------------------
 echo -e ""
@@ -22,7 +22,7 @@ cat > /usr/lib/systemd/scripts/rtc << ENDRTCSCRIPT
 #!/bin/bash
 # create an i2c device DS1307 (works also for DS3231)
 # set systemclock from external i2c-rtc
-echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
+echo 'ds1307 0x68' > /sys/class/i2c-adapter/i2c-1/new_device
 hwclock -s
 ENDRTCSCRIPT
 
